ecbdfe7746b6919477b027dd1daead6a55399f94
Sree varshan <sreevarshanmurali@gmail.com>
2024-03-03 00:15:14 +0530

    made changes in cart model and structured overall code

---
+++ data/products.js
@@ -0,0 +1,78 @@
+const products = [
+  {
+    name: "Candles",
+    imageUrl:
+      "https://img.freepik.com/free-photo/still-life-with-aroma-diffuser-moisturizing-air-burning-candles_169016-13104.jpg?t=st=1709393193~exp=1709396793~hmac=df150ecbf1947cccf0524e2a0a86dd20b521a627427ec286b8a1ee9003e5a1ae&w=1800",
+    description:
+      " A variety of scented candles, including aromatherapy options or decorative ones. ",
+    price: 120,
+    countInStock: 10,
+  },
+  {
+    name: "Amazon Alexa",
+    imageUrl:
+      "https://images.unsplash.com/photo-1518444065439-e933c06ce9cd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
+    description:
+      "It is capable of voice interaction, music playback, making to-do lists, setting alarms, streaming podcasts, playing audiobooks, and providing weather, traffic, sports, and other real-time information, such as news. Alexa can also control several smart devices using itself as a home automation system.",
+    price: 50,
+    countInStock: 25,
+  },
+  {
+    name: "Hand made jewelry",
+    imageUrl:
+      "https://img.freepik.com/free-photo/top-view-essentials-bead-working-with-thread-scissors_23-2148815802.jpg?t=st=1709393787~exp=1709397387~hmac=5ab2ba6d3ea8fcdb6b28d7f3de1de85cfe2ae2ec2e6ed5930912a3a671ef56a5&w=2000",
+    description:
+      "Each piece is meticulously crafted by skilled artisans, showcasing exquisite craftsmanship and attention to detail. From delicate necklaces adorned with sparkling gemstones to bold statement earrings featuring intricate designs, our handmade jewelry embodies beauty, quality, and uniqueness. Made with love and passion",
+    price: 100,
+    countInStock: 5,
+  },
+  {
+    name: "JBL FLIP 4",
+    imageUrl:
+      "https://images.unsplash.com/photo-1564424224827-cd24b8915874?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80",
+    description:
+      "JBL Flip 4 is the next generation in the award-winning Flip series; it is a portable Bluetooth speaker that delivers surprisingly powerful stereo sound. This compact speaker is powered by a 3000mAh rechargeable Li-ion battery that offers up to 12 hours of continuous, high-quality audio playtime.",
+    price: 140,
+    countInStock: 10,
+  },
+
+  {
+    name: "Iphone 12",
+    imageUrl:
+      "https://images.unsplash.com/photo-1605787020600-b9ebd5df1d07?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1463&q=80",
+    description:
+      "Welcome to a new era of iPhone. Beautifully bright 6.1-inch Super Retina XDR display.1 Ceramic Shield with 4x better drop performance.2 Incredible low-light photography with Night mode on all cameras. Cinema-grade Dolby Vision video recording, editing, and playback. Powerful A14 Bionic chip. And new MagSafe accessories for easy attach and faster wireless charging.3 Let the fun begin.",
+    price: 1099,
+    countInStock: 10,
+  },
+  {
+    name: "Cannon EOS-1D",
+    imageUrl:
+      "https://images.unsplash.com/photo-1519183071298-a2962feb14f4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    description:
+      "The EOS-1D X combines speed with image quality, to create the next generation camera for professionals. Full frame 18 megapixel sensor with Dual “DIGIC 5+” processors sets the standard, and up to 12 frames per second shooting takes it beyond.",
+    price: 1300,
+    countInStock: 5,
+  },
+  {
+    name: "PlayStation 5",
+    imageUrl:
+      "https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80",
+    description:
+      "PlayStation 5 (PS5) is a home video game console developed by Sony Interactive Entertainment. Announced in 2019 as the successor to the PlayStation 4, the PS5 was released on November 12, 2020 in Australia, Japan, New Zealand, North America, Singapore, and South Korea, and November 19, 2020 onwards in other major markets except China and India.",
+    price: 499,
+    countInStock: 15,
+  },
+
+  {
+    name: "Audio Technica Headphones",
+    imageUrl:
+      "https://images.unsplash.com/photo-1558756520-22cfe5d382ca?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    description:
+      "Outfitted with 45mm large-aperture dynamic drivers and an over-ear, closed-back design, the ATH-M50x headphones deliver clarity, deep bass, and extended bandwidth (15 Hz to 28 kHz) while isolating you from outside sounds.",
+    price: 233,
+    countInStock: 4,
+  },
+];
+
+module.exports = products;
